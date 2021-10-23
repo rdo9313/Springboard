a3b20818ce70bae57b0806098ec9edf5f5ffc69c
Ryan Do <rdo9313@gmail.com>
2021-10-23 19:58:11 -0400

    terminal exercise

---
+++ Challenges/Terminal_Exercise/Part_1.js
@@ -0,0 +1,17 @@
+// 1
+mkdir first
+// 2
+cd first
+// 3
+touch person.txt
+// 4
+mv person.txt another.txt
+// 5
+cp another.txt copy.txt
+// 6
+rm copy.txt
+// 7
+cp -r first second
+// 8
+rm -rf second
+
---
+++ Challenges/Terminal_Exercise/Part_2.js
@@ -0,0 +1,22 @@
+// 1
+Lists manual for command. 
+// 2
+-l flag lists files with more detail while -a flag lists all files including ones starting with "."
+// 3
+option/alt left/right
+// 4
+ctrl - e
+// 5
+ctrl - a
+// 6
+option delete
+// 7
+A terminal is an interface where the shell executes the commands.
+// 8
+Absolute path describes the location of a file or folder starting from the root route
+// 9
+Relative path describes the location of a file or folder starting from the current location
+// 10
+A flag is used for additional functionality of a command. i.e. -a, -r, and -rf
+// 11
+The r flag recursively removes files from the folder while the f flag forces removal of files
