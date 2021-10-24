4f3cd2d6d13fc211caf7876fd4de851aa82f7e24
Ryan Do <rdo9313@gmail.com>
2021-10-24 11:20:36 -0400

    branching exericse

---
+++ Challenges/branching_exercise.js
@@ -0,0 +1,7 @@
+/*
+1. git checkout -b
+2. fast forwards happen when there are no commits on original branch while recursive happens when there are different commits on both branches.
+3. git checkout
+4. git branch -d/-D
+5. When there are different commits to the same file and Git can't determine which to update with.
+*/
