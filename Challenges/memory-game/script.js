8e1f17f0d7bd15e0f85bf52e4970a9d2f4f95973
Ryan Do <rdo9313@gmail.com>
2021-10-24 08:30:48 -0400

    memory game score system added

--- Challenges/memory-game/script.js
+++ Challenges/memory-game/script.js
@@ -1,7 +1,8 @@
 const gameContainer = document.getElementById("game");
-let count = 0;
-let firstCard;
-let freezeCards = false;
+const playButton = document.querySelector(".play-button");
+const resetButton = document.querySelector(".reset-button");
+const guessScore = document.querySelector("#guess-score");
+const bestScore = document.querySelector("#best-score");
 
 const COLORS = [
   "red",
@@ -15,6 +16,41 @@ const COLORS = [
   "orange",
   "purple"
 ];
+
+let count = 0;
+let score = 0;
+let firstCard;
+let freezeCards = true;
+let shuffledColors = shuffle(COLORS);
+
+playButton.addEventListener("click", e => {
+  const cards = document.querySelectorAll("div");
+  let colors = shuffle(COLORS);
+  for (let i = 0; i < cards.length - 1; i++) {
+    cards[i+1].setAttribute("class", colors[i]);
+  }
+  freezeCards = false;
+  e.target.style.display = "none";
+  resetButton.style.display = "inline-block";
+})
+
+resetButton.addEventListener("click", e => {
+  const cards = document.querySelectorAll("div");
+  if (gameOver(cards)) {
+    if (localStorage.getItem("best-score") === null || score < bestScore.innerText) {
+      localStorage.setItem("best-score", score);
+      displayBestScore();
+    }
+  }
+  for (let i = 1; i < cards.length; i++) {
+    hideCard(cards[i]);
+  }
+  score = 0;
+  guessScore.innerText = score;
+  freezeCards = true;
+  e.target.style.display = "none";
+  playButton.style.display = "inline-block";
+})
 
 // here is a helper function to shuffle an array
 // it returns the same array with values shuffled
@@ -39,7 +75,9 @@ function shuffle(array) {
   return array;
 }
 
-let shuffledColors = shuffle(COLORS);
+function displayBestScore() {
+  bestScore.innerText = localStorage.getItem("best-score") || 0;
+}
 
 // this function loops over the array of colors
 // it creates a new div and gives it a class with the value of the color
@@ -58,6 +96,16 @@ function createDivsForColors(colorArray) {
     // append the div to the element with an id of game
     gameContainer.append(newDiv);
   }
+}
+
+function gameOver(cards) {
+  let count = 0;
+  for (let i = 1; i < cards.length; i++) {
+    if (cards[i].classList.contains("flipped")) {
+      count++;
+    }
+  }
+  return count === 10;
 }
 
 function showCard(card) {
@@ -89,8 +137,11 @@ function handleCardClick(event) {
       freezeCards = false;
     }, 1000)
     count = 0;
+    score++;
+    guessScore.innerText = score;
   }
 }
 
-// when the DOM loads
+
 createDivsForColors(shuffledColors);
+displayBestScore();
